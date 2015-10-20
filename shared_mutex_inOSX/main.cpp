dfc94023fce8e91d4c8a9ba8e37abbd8c3ab286f
pebble8888 <pebble8888@gmail.com>
2015-10-20 22:12:19 +0900

    init

--- shared_mutex_inOSX/main.cpp
+++ shared_mutex_inOSX/main.cpp
@@ -3,13 +3,17 @@
 //  shared_mutex_inOSX
 //
 //  Created by pebble8888 on 2015/10/20.
-//  Copyright © 2015年 pebble8888. All rights reserved.
+//  Copyright © 2015 pebble8888. All rights reserved.
 //
 
 #include <iostream>
+#include "shared_mutex"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::shared_timed_mutex m;
+    m.lock();
+    m.unlock();
+    m.lock_shared();
+    m.unlock_shared();
     return 0;
 }
